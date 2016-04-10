d649ea05255bde6d35e48188dd6042774949351d
chunguiLiu <404468494@qq.com>
2016-04-10 15:20:36 +0800

    add

---
+++ src/main/java/com/example/user/androidintent/IntentModule.java
@@ -0,0 +1,206 @@
+package com.initproject;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.util.Log;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.io.File;
+import java.util.List;
+
+public class IntentModule extends ReactContextBaseJavaModule{
+    protected static final String TAG = IntentModule.class.getSimpleName();
+
+    public IntentModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "Intent";
+    }
+
+    @ReactMethod
+    public void open(String filePath, Callback callback) {
+        File file = new File(filePath);
+        Intent intent = null;
+        Log.i(TAG, "open method");
+        if(!file.exists()) return;
+        /* 取得扩展名 */
+        String end = file.getName().substring(file.getName().lastIndexOf(".") + 1,file.getName().length()).toLowerCase();
+        /* 依扩展名的类型决定MimeType */
+        if(end.equals("m4a")||end.equals("mp3")||end.equals("mid")||
+                end.equals("xmf")||end.equals("ogg")||end.equals("wav")){
+            intent = getAudioFileIntent(filePath);
+        }
+        else if(end.equals("3gp")||end.equals("mp4")){
+            intent = getAudioFileIntent(filePath);
+        }
+        else if(end.equals("jpg")||end.equals("gif")||end.equals("png")||
+                end.equals("jpeg")||end.equals("bmp")){
+            intent = getImageFileIntent(filePath);
+        }
+        else if(end.equals("ppt")){
+            intent = getPptFileIntent(filePath);
+        }
+        else if(end.equals("xls")){
+            intent = getExcelFileIntent(filePath);
+        }
+        else if(end.equals("doc")){
+            intent = getWordFileIntent(filePath);
+        }
+        else if(end.equals("pdf")){
+            intent = getPdfFileIntent(filePath);
+        }
+        else if(end.equals("chm")){
+            intent = getChmFileIntent(filePath);
+        }
+        else if(end.equals("txt")){
+            intent = getTextFileIntent(filePath,false);
+        }
+        else if(end.equals("docx")) {
+            intent = getDocxFileIntent(filePath);
+        }
+        Log.i(TAG, filePath);
+        //打开
+        if(intent != null && isIntentAvailable(intent)) {
+            Log.i(TAG, "可打开");
+            getReactApplicationContext().startActivity(intent);
+            callback.invoke(true);
+        }
+        else {
+            Log.i(TAG, "不可打开");
+            callback.invoke(false);
+        }
+    }
+
+    // 可以判断本地是否存在如office,pdf 等
+    public boolean isIntentAvailable(Intent intent) {
+        final PackageManager packageManager = getReactApplicationContext().getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+                PackageManager.GET_ACTIVITIES);
+        return list.size() > 0;
+    }
+
+    // android获取一个用于打开HTML文件的intent  
+    private Intent getHtmlFileIntent(String param) {
+        Uri uri = Uri.parse(param).buildUpon().encodedAuthority("com.android.htmlfileprovider").scheme("content").encodedPath(param).build();
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.setDataAndType(uri, "text/html");
+        return intent;
+    }
+
+    // android获取一个用于打开图片文件的intent  
+    private Intent getImageFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "image/*");
+        return intent;
+    }
+
+    // android获取一个用于打开PDF文件的intent  
+    private Intent getPdfFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/pdf");
+        return intent;
+    }
+
+    // android获取一个用于打开文本文件的intent  
+    private Intent getTextFileIntent(String param, boolean paramBoolean) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (paramBoolean) {
+            Uri uri1 = Uri.parse(param);
+            intent.setDataAndType(uri1, "text/plain");
+        } else {
+            Uri uri2 = Uri.fromFile(new File(param));
+            intent.setDataAndType(uri2, "text/plain");
+        }
+        return intent;
+    }
+
+    // android获取一个用于打开音频文件的intent  
+    private Intent getAudioFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "audio/*");
+        return intent;
+    }
+
+    // android获取一个用于打开视频文件的intent  
+    private Intent getVideoFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "video/*");
+        return intent;
+    }
+
+    // android获取一个用于打开CHM文件的intent  
+    private Intent getChmFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/x-chm");
+        return intent;
+    }
+
+    // android获取一个用于打开Word文件的intent  
+    private Intent getWordFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/msword");
+        return intent;
+    }
+
+    //打开docx文件
+    private Intent getDocxFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        return intent;
+    }
+
+    // android获取一个用于打开Excel文件的intent  
+    private Intent getExcelFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/vnd.ms-excel");
+        return intent;
+    }
+
+    // android获取一个用于打开PPT文件的intent  
+    private Intent getPptFileIntent(String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        return intent;
+    }
+
+}
